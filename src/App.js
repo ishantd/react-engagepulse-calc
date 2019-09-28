040bc6449ee6de30c19fa19485d36b8c9b080bfd
ishant <ishantdahiya@gmail.com>
2019-09-29 01:52:28 +0530

    UI Complete for Calc app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
+import ClearButton from './components/ClearButton'
 
-function App() {
-  return (
-    <div className="app">
-      React
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      input: ""
+    };
+  } 
+  render () {
+
+    return (
+      <div className="app">
+      <div className="calc-wrapper">
+        <Input input={this.state.input}></Input>
+          <div className="row">
+              <Button>7</Button>
+              <Button>8</Button>
+              <Button>9</Button>
+              <Button>/</Button>
+          </div>
+          <div className="row">
+              <Button>4</Button>
+              <Button>5</Button>
+              <Button>6</Button>
+              <Button>X</Button>
+          </div>
+          <div className="row">
+              <Button>1</Button>
+              <Button>2</Button>
+              <Button>3</Button>
+              <Button>+</Button>
+          </div>
+          <div className="row">
+              <Button>.</Button>
+              <Button>0</Button>
+              <Button>=</Button>
+              <Button>-</Button>
+          </div>
+          <div className="row">
+            <ClearButton handleClear={()=> this.setState({ input: '' })}>
+              Clear
+            </ClearButton>
+          </div>
+      </div>
     </div>
   );
 }
 
+}
 export default App;
---
+++ src/components/ClearButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ClearButton.css'
+
+const ClearButton = (props) => (
+    <div className="clear-btn" onClick={props.handleClear}>
+        {props.children}
+    </div>
+)
+
+
+export default ClearButton;
