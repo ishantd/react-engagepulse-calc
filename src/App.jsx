6b15f0ed0100e5de9e342c40d673ef14022b31e5
ishant <ishantdahiya@gmail.com>
2019-09-29 05:23:13 +0530

    v1 final, dark mode beta

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,26 @@ import Button from './components/Button';
 import Input from './components/Input';
 import ClearButton from './components/ClearButton'
 import { evaluate } from 'mathjs';
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
 // import { toggleSc } from './togglesc'
 
 
-const GlobalStyle = createGlobalStyle `
-body {
-  background-color: ${props =>
-  props.theme.mode === 'dark' ? '#000' : '#fff'};
-  color: ${props =>
-  props.theme.mode === 'dark' ? '#fff' : '#000'};
-}
-.button-wrapper {
-  background-color: ${props =>
-  props.theme.mode === 'dark' ? '#666' : '#f0f0f0' };
-  color: ${props =>
-  props.theme.mode === 'dark' ? '#fff' : '#000'}
-}
-`
+// const GlobalStyle = createGlobalStyle `
+// body {
+//   background-color: ${props =>
+//   props.theme.mode === 'dark' ? '#000' : '#fff'};
+//   color: ${props =>
+//   props.theme.mode === 'dark' ? '#fff' : '#000'};
+// }
+// .button-wrapper {
+//   background-color: ${props =>
+//   props.theme.mode === 'dark' ? '#666' : '#f0f0f0' };
+//   color: ${props =>
+//   props.theme.mode === 'dark' ? '#fff' : '#000'}
+// }
+// `
 
 class App extends Component {
+  
   constructor(props) {
     super(props);
     
@@ -52,38 +52,62 @@ class App extends Component {
     this.setState( {input: this.state.input*this.state.input} )
   }
 
+  
   render () {
-
+    
+    
+    
+    
     return (
+      
 
+      
 
-      <ThemeProvider theme={{ mode: 'dark' }}>
-        <>
-        <GlobalStyle />
-
-
+      
       <div className="app">
-      <div className="calc-wrapper">
+        <div className="dl">
+          <Button handleClick={  () =>  {
+            var x = this.refs.row;
+            if (document.body.style.backgroundColor === "black") {
+              document.body.style.backgroundColor = "white";
+            } else {
+              // x.style.backgroundColor = "black";
+              document.body.style.backgroundColor = "black";
+            }
+            
+            if (x.style.backgroundColor === "white") {
+              x.style.backgroundColor = "black"
+              x.style.color = "white"
+            }
+            else {
+              x.style.backgroundColor = "white"
+              x.style.color = "black"
+            }
+
+          }
+      }>Dark Mode/ Light Mode</Button>
+          </div>
+      <div className="calc-wrapper" >
         <Input input={this.state.input}></Input>
-          <div className="row">
+          <div className="row" id="row" ref="row">
               <Button handleClick={this.addToInput}>7</Button>
               <Button handleClick={this.addToInput}>8</Button>
               <Button handleClick={this.addToInput}>9</Button>
               <Button handleClick={this.addToInput}>/</Button>
           </div>
-          <div className="row">
+          <div className="row" id="row" ref="row">
               <Button handleClick={this.addToInput}>4</Button>
               <Button handleClick={this.addToInput}>5</Button>
               <Button handleClick={this.addToInput}>6</Button>
               <Button handleClick={this.addToInput}>*</Button>
           </div>
-          <div className="row">
+          <div className="row" id="row" ref="row">
               <Button handleClick={this.addToInput}>1</Button>
               <Button handleClick={this.addToInput}>2</Button>
               <Button handleClick={this.addToInput}>3</Button>
               <Button handleClick={this.addToInput}>+</Button>
           </div>
-          <div className="row">
+          <div className="row" id="row" ref="row">
           <Button handleClick={  () =>  {
             var x = this.refs.scrow;
             if (x.style.display === "none") {
@@ -103,18 +127,18 @@ class App extends Component {
               <Button handleClick={()=>this.handleSq()}>x^2</Button>
               <Button handleClick={()=>this.handleSqrt()}>(x)^(1/2)</Button>
           </div>
-          <div className="row">
+          <div className="row" id="row" ref="row">
             <ClearButton handleClear={()=> this.setState({ input: '' })}>
               Clear
             </ClearButton>
           </div>
+          
+          
       </div>
     </div>
 
-    </>
-
-    </ThemeProvider>
-  );
+    
+  ); // Return
 }
 
 }
