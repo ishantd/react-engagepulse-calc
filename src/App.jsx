c619972a5222b1737e54285b3a9cf061376aa9fb
ishant <ishantdahiya@gmail.com>
2019-09-29 03:03:54 +0530

    added toggle for Scientific Calculator

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
+import ClearButton from './components/ClearButton'
+// import { toggleSc } from './togglesc'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      input: "0"
+    };
+  } 
+
+  addToInput = val => {
+    this.setState({ input: this.state.input + val });
+  }
+
+  handleEqual = () => {
+    this.setState({input: this.state.input})
+  }
+
+  render () {
+
+    return (
+      <div className="app">
+      <div className="calc-wrapper">
+        <Input input={this.state.input}></Input>
+          <div className="row">
+              <Button handleClick={this.addToInput}>7</Button>
+              <Button handleClick={this.addToInput}>8</Button>
+              <Button handleClick={this.addToInput}>9</Button>
+              <Button handleClick={this.addToInput}>/</Button>
+          </div>
+          <div className="row">
+              <Button handleClick={this.addToInput}>4</Button>
+              <Button handleClick={this.addToInput}>5</Button>
+              <Button handleClick={this.addToInput}>6</Button>
+              <Button handleClick={this.addToInput}>X</Button>
+          </div>
+          <div className="row">
+              <Button handleClick={this.addToInput}>1</Button>
+              <Button handleClick={this.addToInput}>2</Button>
+              <Button handleClick={this.addToInput}>3</Button>
+              <Button handleClick={this.addToInput}>+</Button>
+          </div>
+          <div className="row">
+          <Button handleClick={  () =>  {
+            var x = this.refs.scrow;
+            if (x.style.display === "none") {
+              x.style.display = "flex";
+            } else {
+              x.style.display = "none";
+            }
+          }
+    
+                }>Scientific</Button>
+              <Button handleClick={this.addToInput}>0</Button>
+              <Button handleClick={() => this.handleEqual}>=</Button>
+              <Button handleClick={this.addToInput}>-</Button>
+          </div>
+          <div className="scrow" id="scrow" ref="scrow">
+              <Button handleClick={this.addToInput}>+/-</Button>
+              <Button handleClick={this.addToInput}>x^2</Button>
+              <Button handleClick={() => this.handleEqual}>(x)^(1/2)</Button>
+          </div>
+          <div className="row">
+            <ClearButton handleClear={()=> this.setState({ input: '' })}>
+              Clear
+            </ClearButton>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+}
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const isOperator = val => {
 const Button = props => (
     <div className={ `button-wrapper ${
         isOperator(props.children) ? null: "operator"
-    }` }>
+    }` } onClick={()=>props.handleClick(props.children)}>
         {props.children}
     </div>
 )
